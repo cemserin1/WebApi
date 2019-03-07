8043875137b35cb7a027a1d02e603f9de32185da
Cem Serin <ocemserin@gmail.com>
2019-03-07 14:10:00 +0300

    products view

---
+++ InGame/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InGame.Api.Helpers;
+using InGame.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InGame.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Uri Uri;
+
+        public ProductController()
+        {
+            Uri = new Uri("https://localhost:44358");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var productList = await RestHelper.GetObjects<ProductResponseModel>(new Uri(Uri, "/api/product"));
+            //var childCategories = await RestHelper.GetObjects<CategoryResponseModel>(new Uri(Uri, "/api/category/GetChildCategories?categoryId=2"));
+            return View(productList);
+        }
+    }
+}
